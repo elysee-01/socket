27fecabccac1536a6c32701865d68648360de460
ELYSEE KEVIN <elyseekevin49@gmail.com>
2021-07-02 11:04:08 +0000

    port

--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello World!');
     res.end();
-}).listen(8080)
+}).listen(process.env.PORT || 8080)
 
 const wss = new WebSocket.Server({
     server: server
